8a4f19cf3569e5d629a3c518d318a787858b8db6
Soko77788 <NickSokolowski7@gmail.com>
2024-03-30 21:53:49 -0500

    elements selected and some functional functions made for getting input data and localStorage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,11 @@
+const $taskForm = $('#taskForm');
+const $taskTitleInput = $('#taskTitle');
+const $taskDueDateInput = $('#taskDueDate');
+const $taskDescriptionInput = $('#taskDescription');
+const $taskModal = $('#formModal')
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+// let taskList = JSON.parse(localStorage.getItem("tasks"));
+// let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 
 
@@ -10,8 +15,8 @@ function generateTaskId() {
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
+function createTaskCard(taskTitle, taskDueDate, taskDescription) {
+    
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -34,7 +39,58 @@ function handleDrop(event, ui) {
 
 }
 
+
+function loadProjectsFromLocalStorage() {
+    const savedProjects = JSON.parse(localStorage.getItem('tasks')) || []
+    return savedProjects
+}
+
+function saveProjectsToLocalStorage(projectsData) {
+    localStorage.setItem('tasks', JSON.stringify(projectsData))
+}
+
+function handleTaskFormSubmit(event) {
+event.preventDefault();
+$taskModal.modal('hide');
+
+// get form field values
+const taskTitle = $taskTitleInput.val();
+const taskDueDate = $taskDueDateInput.val();
+const taskDescription = $taskDescriptionInput.val();
+
+// console.log(taskTitle, taskDueDate, taskDescription)
+
+// get current saved projects
+const projects = loadProjectsFromLocalStorage()
+
+// new project data
+const newProject = {
+    title: taskTitle,
+    dueDate: taskDueDate,
+    description: taskDescription
+}
+
+// add new project to the list of savedProject
+projects.push(newProject)
+
+// save cards to localStorage
+saveProjectsToLocalStorage(projects)
+
+//create a card
+// re-render cards in their lanes
+
+
+// reset form
+$taskTitleInput.val('')
+$taskDueDateInput.val('')
+$taskDescriptionInput.val('')
+
+
+}
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+$taskForm.on('submit', handleTaskFormSubmit)
 });
+
+
+
