d432e71c266a5916392dd0ae432e30cc1eff0d90
Soko77788 <NickSokolowski7@gmail.com>
2024-04-02 17:22:14 -0500

    colors changing according to date but done lane not turning white

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,16 +4,15 @@ const $taskTitleInput = $('#taskTitle');
 const $taskDueDateInput = $('#taskDueDate');
 const $taskDescriptionInput = $('#taskDescription');
 const $taskModal = $('#formModal')
-console.log($swimLanesContainer, $taskForm, $taskTitleInput, $taskDueDateInput, $taskDescriptionInput, $taskModal)
+
 //list selections
 const $todoList = $('#todo-cards');
 const $inProgressList = $('#in-progress-cards');
 const $doneList = $('#done-cards');
-console.log($todoList, $inProgressList, $doneList)
 
-// Retrieve tasks and nextId from localStorage
-// let taskList = JSON.parse(localStorage.getItem("tasks"));
-// let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Retrieve tasks from localStorage
+
 
 function loadProjectsFromLocalStorage() {
     const savedProjects = JSON.parse(localStorage.getItem('tasks')) || []
@@ -24,47 +23,52 @@ function saveProjectsToLocalStorage(projectsData) {
     localStorage.setItem('tasks', JSON.stringify(projectsData))
 }
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
 
-}
 
 // Todo: create a function to create a task card
-function createTaskCard(taskTitle, taskDueDate, taskDescription) {
-    
-}
 
-//create card
 function createCardEl(projectData) {
+    const dueDate = dayjs(projectData.dueDate);
+    const today = dayjs();
+    
+    console.log(dueDate)
+
+    let colorClass = '';
+    if (dueDate.isBefore(today, 'day')) {
+        colorClass = 'bg-danger'; // Past due date, red background
+    } else if (dueDate.isSame(today, 'day')) {
+        colorClass = 'bg-warning'; // Due today, yellow background
+    } else {
+        colorClass = 'bg-white'; // Future due date or in the done lane, white background
+    }
+
     const card = $(`
-    <div class="card draggable" data-id='${projectData.id}' data-status='${projectData.status}' style="width: 18rem;">
+    <div class="card draggable ${colorClass}" data-id='${projectData.id}' data-status='${projectData.status}' style="width: 18rem;">
     <div class="card-body"> 
     <h5 class="card-title">${projectData.title}</h5>
     <h6 class="card-subtitle mb-2 text-muted">${projectData.dueDate}</h6>
     <p class="card-text">${projectData.description}</p>
     <button class="btn btn-danger delete-card">Delete</button>
-   
-  </div>
-</div>
+    
+    </div>
+    </div>
     `)
     return card
 }
 
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList(event, ui) {
-}
 
 function renderCardsToLists() {
     const savedProjects = loadProjectsFromLocalStorage()
-
+    
     $todoList.empty()
     $inProgressList.empty()
     $doneList.empty()
-
+    
     for (const projectData of savedProjects) {
         const cardEl = createCardEl(projectData)
-
+        
         if (projectData.status === 'todo') {
             $todoList.append(cardEl)
         } else if (projectData.status === 'in-progress') {
@@ -73,17 +77,15 @@ function renderCardsToLists() {
             $doneList.append(cardEl)
         }
     }
-
+    
     $('.draggable').draggable({
         stack: '.swim-lanes'
     })
 }
 
 
+// Todo: create a function to generate a unique task id
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
 
 function handleTaskFormSubmit(event) {
     event.preventDefault();
@@ -94,7 +96,6 @@ function handleTaskFormSubmit(event) {
     const taskDueDate = $taskDueDateInput.val();
     const taskDescription = $taskDescriptionInput.val();
     
-    // console.log(taskTitle, taskDueDate, taskDescription)
     
     // get current saved projects
     const projects = loadProjectsFromLocalStorage()
@@ -114,16 +115,12 @@ function handleTaskFormSubmit(event) {
     // save cards to localStorage
     saveProjectsToLocalStorage(projects)
     
-    //create a card
-    // re-render cards in their lanes
     
     renderCardsToLists()
     // reset form
     $taskTitleInput.val('')
     $taskDueDateInput.val('')
     $taskDescriptionInput.val('')
-    
-    
     
     }
 
@@ -141,7 +138,13 @@ function handleDrop(event, ui) {
         // find object in saved Project with same id
         if (projectData.id === projectId) {
             // update it's status to the target swim-lanes id
-            projectData.status = targetListId
+            projectData.status = targetListId;
+
+            if (targetListId === 'done') {
+                // Set the background color of the card to white
+                $(card).find('.card-body').css('background-color', 'white');
+                console.log(targetListId)
+            } 
         }
     }
 
@@ -153,10 +156,6 @@ function handleDrop(event, ui) {
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
-
 function deleteCard(event) {
     const cardId = $(event.target).closest('.card').data('id')
 console.log(cardId)
@@ -174,16 +173,6 @@ saveProjectsToLocalStorage(projectsToKeep)
 
 renderCardsToLists()
 }
-
-
-
-
- 
-
-
-
-
-
 
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
